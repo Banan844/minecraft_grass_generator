c1dd997c8b9ddcb5b4b37a65897b8d2c26fddd0b
Neonan3442 <148681974+Banan844@users.noreply.github.com>
2026-02-25 22:31:41 +0300

    v2.0 update
    Code simplifying, adding more commentaries. New minecraft grass pallete generation method.

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Minecraft grass texture generator
-Generates gray gradient colors from 117 to 192 *(can be easy changed in code)*. After that, generating image, sized 16x16 px *(also can be easy changed)*, where randomly choices pixel from gradient.
+Wide-customizable minecraft grass texture generator. Randomly choices colors from pallete to image. Has two pallete generation methods. Better method is minecraft pallete, he is more similar to minecraft grass texture. 
+
+Pallete generation methods:
+1. Simple gradient *(gradient)*: Generates gray gradient colors from 117 to 192.
+2. Minecraft pallete *(pallete)*: Already specified pallete from minecraft gray grass colors. Almost as simple gradient pallete, but hasn't some colors.
+
 ## Dependencies
 Requires: [**PyPNG**](https://pypi.org/project/pypng/ "See on PyPI"). Install with: `pip install pypng`.
 ## Usage
-Starting without arguments creates image `image.png` in folder, where you writed command, random seed is selecting by python random. If you started program with random seed argument, all at same, but image generating with your random seed.
+Starting without arguments creates image in folder, where you writed command. Random seed is selecting by python random. If you started program with random seed argument, all at same, but image generating with your random seed. All parameters (size, pallete method) you can change at top the code.
+
+**Filename** is seed, numeration, pallete method. Example: `seed1234_3442_pallete.png`
 
 **Program arguments**: random seed *(not required, type: integer)*.
 
@@ -16,7 +23,11 @@ Start with random seed argument *(here random seed is **123**)*:
 ```bash
 python main.py 123
 ```
-## Example
-Image generated with seed 1 *(scaled)*:
 
-![Example image](example_image.png "Generated image with seed 1")
+Image generated with seed 1 and gradient method *(scaled)*:
+
+![Example image 1](example_image1.png "Generated image with seed 1")
+
+Image generated with seed 1, but pallete method *(scaled)*:
+
+![Example image 2](example_image2.png "Generated image with seed 1")
--- main.py
+++ main.py
@@ -1,27 +1,66 @@
 import png
 import random
+import os
+from time import time
 
 
+# Parameters
 width = 16
 height = 16
 
 
+method = "pallete"
 
+
+
+# Image generation main function
 def main(seed: int | None = None):
+
+    # Check seed parameter for existing
     if type(seed) == int:
        random.seed(seed)
 
-    gradient = [117, 192]
-    colors = [
-        (gradient[0], gradient[0], gradient[0])
-    ]
+
+    # Gradient method
+    if method == "gradient":
+        gradient = [117, 192] # Two colors for gradient
+        colors = [
+            (gradient[0], gradient[0], gradient[0]) # Convert to RGB
+        ]
+
+        
+        color_ = gradient[0] # First color
+        for i in range(gradient[1] - color_): # Iterages into first color and second color
+            color_result_ = color_+i+1 # Color is depends to iterage
+            color_result_ = (color_result_, color_result_, color_result_) # Convert to RGB
+            colors.append(color_result_) # Append color
+
+        colors_ = colors.copy()
+        
+        # Multiplying colors, while they not fill full image (see later)
+        while len(colors) < width*height:
+            colors.extend(colors_)
 
 
-    color_ = gradient[0]
-    for i in range(gradient[1] - color_):
-        color_result_ = color_+i+1
-        color_result_ = (color_result_, color_result_, color_result_)
-        colors.append(color_result_)
+    # Minecraft grass pallete method
+    elif method == "pallete":
+        
+        # Gray colors pallete
+        colors_ = [112, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 179, 181, 182, 183, 186, 191, 192, 195]
+
+
+        colors = []
+
+        # Create colors, while they not fill full image (see later)
+        while len(colors) < width*height:
+              
+            # Convert to rgb
+            for color in colors_:
+                colors.append((color, color, color))
+    
+    else:
+        print(f"Incorrect method: {method}. Acceptable methods: gradient, pallete.")
+        raise SystemExit(1)
 
 
     image_data = []
@@ -32,8 +71,27 @@ def main(seed: int | None = None):
         row = []
 
         for y in range(height):
-            val = colors[ random.randint(0, len(colors)-1) ]
-            row.extend(val)
+
+            val_idx = random.randint(0, len(colors)-1) # Random color index
+            val = colors[ val_idx ] # Get this color
+            row.extend(val) # Append this color
+            
+            
+            # Remove used color, for he is not repeats (for that colors needs to fill image)
+
+            colors_ = colors.copy() # Copy colors
+            i = 0 # Index
+
+            colors = [] # Remove old colors
+            
+            for color in colors_:
+                
+                # Append color, if he is not used color
+                if i != val_idx:
+                   colors.append(color)
+                
+                i += 1 # Update index
+
 
         image_data.append(row)
     
@@ -41,9 +99,8 @@ def main(seed: int | None = None):
 
 
 
-
-def save( image_data: list[ list[int] ] ):
-    filename = "image.png"
+# Save image
+def save( image_data: list[ list[int] ], filename: str ):
 
     # Open a file in binary write mode
     with open(filename, 'wb') as f:
@@ -53,11 +110,13 @@ def save( image_data: list[ list[int] ] ):
         w.write(f, image_data)
 
 
-    print(f"Image {filename}  successfully.")
+    print(f"Image {filename} successfully saved.")
 
 
 
+# Checks for arguments
 
+# Check for integer type
 def is_int(value):
     try:
         int(value)
@@ -65,7 +124,8 @@ def is_int(value):
     except:
         return False
 
-def is_idx(list_: list, idx: int):
+# Check index existing in list
+def has_idx(list_: list, idx: int):
     try:
         list_[idx]
         return True
@@ -74,12 +134,24 @@ def is_idx(list_: list, idx: int):
 
 
 
+
+# File start
 if __name__ == "__main__":
    from sys import argv
 
    seed = None
+   seed_idx = 1
 
-   if is_idx(argv, 1) and is_int( argv[1] ):
-      seed = int(argv[1])
+   filename = "image.png"
 
-   save( main(seed) )
+
+   if has_idx( argv, seed_idx ) and is_int( argv[seed_idx] ):
+      seed = int( argv[seed_idx] )
+   else:
+      t = int(time())
+      seed = int( t + (  int.from_bytes( os.urandom(16), 'big' ) % pow( 10, len(str(t)) )  ) )
+
+
+   filename = f"seed{seed}_{ int( random.randint(0, 4999) + ( time() % 4999 ) ) }_{method}.png"
+
+   save( main(seed), filename )
